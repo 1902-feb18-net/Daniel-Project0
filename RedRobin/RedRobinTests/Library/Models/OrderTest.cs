696ffbf6f81d7e44c2c50cd1d9abf50e2b9ce4ce
Daniel Rendon <danielrendon93@gmail.com>
2019-03-13 15:00:26 -0500

    comment code

--- RedRobin/RedRobinTests/Library/Models/OrderTest.cs
+++ RedRobin/RedRobinTests/Library/Models/OrderTest.cs
@@ -12,35 +12,35 @@ namespace RedRobinTests.Library.Models
     {
         readonly RedRobin.Library.Models.Orders order = new RedRobin.Library.Models.Orders();
 
-        [Fact]
-        public void Add_Order()
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<RedRobinContext>();
-            optionsBuilder.UseSqlServer(SecretConfiguration.ConnectionString);
-            var options = optionsBuilder.Options;
-            bool ordAdded = false;
+        //[Fact]
+        //public void Add_Order()
+        //{
+        //    var optionsBuilder = new DbContextOptionsBuilder<RedRobinContext>();
+        //    optionsBuilder.UseSqlServer(SecretConfiguration.ConnectionString);
+        //    var options = optionsBuilder.Options;
+        //    bool ordAdded = false;
 
-            var dbContext = new RedRobinContext(options);
+        //    var dbContext = new RedRobinContext(options);
 
-            IRedRobinRepo redRobinRepository = new RedRobinRepo(dbContext);
+        //    IRedRobinRepo redRobinRepository = new RedRobinRepo(dbContext);
 
-            order.CostTotal = 13;
-            order.orderDate = new DateTime(2019,3,3);
-            order.restaurantID = 11;
-            order.cutomerID = 9;
+        //    order.CostTotal = 13;
+        //    order.orderDate = new DateTime(2019,3,3);
+        //    order.restaurantID = 11;
+        //    order.cutomerID = 9;
 
-            redRobinRepository.AddOrders(order);
+        //    redRobinRepository.AddOrders(order);
 
-            dbContext.SaveChanges();
+        //    dbContext.SaveChanges();
 
-            foreach (var item in redRobinRepository.GetAllOrdersDB())
-            {
-                if (item.orderDate == new DateTime(2019,3,3))
-                {
-                    ordAdded = true;
-                }
-            }
-            Assert.True(ordAdded);
-        }
+        //    foreach (var item in redRobinRepository.GetAllOrdersDB())
+        //    {
+        //        if (item.orderDate == new DateTime(2019,3,3))
+        //        {
+        //            ordAdded = true;
+        //        }
+        //    }
+        //    Assert.True(ordAdded);
+        //}
     }
 }
